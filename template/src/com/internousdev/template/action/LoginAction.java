d64208191004886f907cc22ef4e26c6846fc7bb7
yu-program <12ym.nkmr.11ym@gmail.com>
2020-01-31 15:17:56 +0900

    1月31日-2

--- template/src/com/internousdev/template/action/LoginAction.java
+++ template/src/com/internousdev/template/action/LoginAction.java
@@ -6,6 +6,7 @@ import org.apache.struts2.interceptor.SessionAware;
 
 import com.internousdev.template.dao.BuyItemDAO;
 import com.internousdev.template.dao.LoginDAO;
+import com.internousdev.template.dto.BuyItemDTO;
 import com.internousdev.template.dto.LoginDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -23,10 +24,20 @@ public class LoginAction extends ActionSupport implements SessionAware{
 
 		result = ERROR;
 		//getLoginUserInfoを呼び出す。
+		//loginId,loginPassword,userNameがloginDTOの中に入ってる
 		loginDTO = loginDAO.getLoginUserInfo(loginUserId,loginPassword);
 		//sessionに格納
 		//Mapのkey(="loginUser")は次sessionを使うときのkeyになるので、任意の名前で良い。
 		session.put("loginUser", loginDTO);
+
+		//ログインが認証されたら、商品情報を表示。
+		//sessionから取り出した値をLoginDTO型に変換。
+		if(((LoginDTO)session.get("loginUser")).getLoginFlg()) {
+			result =SUCCESS;
+			BuyItemDTO buyItemDTO =new BuyItemDAO.getBuyItemInf();
+
+			session.put("login_user_id",loginDTO.getLoginId());
+		}
 
 	}
 
